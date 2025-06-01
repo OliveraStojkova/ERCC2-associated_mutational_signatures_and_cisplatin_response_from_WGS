692178e842fa25b57e9b4b7df3e00243e61c6011
OliveraStojkova <stojkovaolivera@outlook.com>
2025-06-01 18:03:03 +0200

    Update NMF_for_DBS.R

--- NMF_for_DBS.R
+++ NMF_for_DBS.R
@@ -24,3 +24,36 @@ nmf_res_dbs_treated <- rename_nmf_signatures(nmf_res_dbs_treated, dbs_signatures
 colnames(nmf_res_dbs_treated$signatures) <- c("DBS-A", "DBS5-like")
 rownames(nmf_res_dbs_treated$contribution) <- c("DBS-A", "DBS5-like")
 
+# Visualize the profile of the signature
+plot_main_dbs_contexts(nmf_res_dbs_treated$signatures, same_y = TRUE)+
+  scale_fill_manual(values = dbs_palette[c(1, 3, 6, 10, 13, 16, 19, 22, 25, 28, 30)]) + 
+  theme(legend.position = "none") + 
+  ylab("Signature weight")
+
+# Cosine similarity between the 2 signatures
+cos_sim(nmf_res_dbs_treated$signatures[, 2], nmf_res_dbs_treated$signatures[,1]) 
+
+# Relative contribution plot
+plot_contribution(nmf_res_dbs_treated$contribution, nmf_res_dbs_treated$signature,
+  mode = "relative", palette = dbs_palette[c(28, 6)]
+) + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Original vs reconstructed plot
+orig_vs_recons_dbs_treated <- cos_sim_matrix(dbs_matrix_treated, nmf_res_dbs_treated$reconstructed)
+cos_sim_values_dbs_treated <- diag(orig_vs_recons_dbs_treated)
+
+cos_sim_df_dbs_treated <- data.frame(Sample = colnames(orig_vs_recons_dbs_treated), Cosine_Similarity = cos_sim_values_dbs_treated)
+
+ggplot(cos_sim_df_dbs_treated, aes(x = Sample, y = Cosine_Similarity)) +
+  geom_bar(stat = "identity", fill = dbs_palette[9]) + 
+  labs(title = "Cosine Similarity Between Original and Reconstructed Vectors for Each Sample",
+       x = "Sample", y = "Cosine Similarity") +
+  geom_hline(yintercept = 0.95, linetype = 'dashed', color = "black") +  
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10), 
+    panel.grid.major = element_blank(),  
+    panel.grid.minor = element_blank())
+
+
+                                                                         
