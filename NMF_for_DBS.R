4f1d1f326235f2b575fc151719ecbbcd78320db9
OliveraStojkova <stojkovaolivera@outlook.com>
2025-06-01 18:00:56 +0200

    Update NMF_for_DBS.R

--- NMF_for_DBS.R
+++ NMF_for_DBS.R
@@ -1,1 +1,26 @@
+# Separate treated and untreated
+dbs_matrix_treated <- dbs_matrix[,grep("cis", colnames(dbs_matrix))]
+dbs_matrix_untreated <- dbs_matrix[, grep("untreated", colnames(dbs_matrix))]
+
+# Check the number of mutations per sample
+colSums(dbs_matrix_treated)
+colSums(dbs_matrix_untreated) --> Relatively low, might cause unreliable results --> NMF only on the treated set
+
+dbs_matrix_treated_w_pseudocount <- dbs_matrix_treated + 0.0001
+
+estimate_dbs_treated <- nmf(dbs_matrix_treated_w_pseudocount, rank = 2:8, method = "brunet", 
+                nrun = 10, seed = 123456, .opt = "v-p")
+
+plot(estimate_dbs_treated)
+
+# Extract signatures
+nmf_res_dbs_treated <- extract_signatures(dbs_matrix_treated_w_pseudocount, rank = 2, nrun = 10, single_core = TRUE)
+
+# Get reference COSMIC signatures
+dbs_signatures <- get_known_signatures(muttype = "dbs", genome = "GRCh38")
+
+# Rename the signatures extracted by NMF 
+nmf_res_dbs_treated <- rename_nmf_signatures(nmf_res_dbs_treated, dbs_signatures, cutoff = 0.8) 
+colnames(nmf_res_dbs_treated$signatures) <- c("DBS-A", "DBS5-like")
+rownames(nmf_res_dbs_treated$contribution) <- c("DBS-A", "DBS5-like")
 
