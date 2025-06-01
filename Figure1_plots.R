4f1961a0e10b111fbec575839e96b7327612eff5
OliveraStojkova <stojkovaolivera@outlook.com>
2025-06-01 17:30:27 +0200

    Create Figure1_plots.R

---
+++ Figure1_plots.R
@@ -0,0 +1,47 @@
+library(paletteer)
+
+color_palette <- paletteer_d("RColorBrewer::RdGy")
+new_color_palette <- paletteer_c("ggthemes::Red-Black Diverging", 30)
+c_palette <- paletteer_c("grDevices::Fall", 30)
+
+# Total number of mutations per sample plot
+mutation_counts_all_long <- mutation_counts_all %>%
+  mutate(sample_name = factor(sample_name, levels = ordered_samples))
+
+ggplot(mutation_counts_all_long, aes(x = sample_name, 
+                                y = num_mutations)) +
+  geom_col(fill = color_palette[2]) +
+  geom_hline(yintercept = mean(mutation_counts_all$num_mutations), color = "black", linetype = "dashed")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 11), panel.grid.major = element_blank(),  
+    panel.grid.minor = element_blank()) +
+  xlab(" ") +
+  ylab("Mutation count")
+
+
+# INS/DEL counts plot
+mutation_counts_by_type_ins_del <- mutation_counts_by_type %>% filter(type %in% c("INS", "DEL"))
+mutation_counts_by_type_ins_del$sample_name <- factor(mutation_counts_by_type_ins_del$sample_name,
+                                               levels = unique(mutation_counts_by_type_ins_del$sample_name))
+
+ordered_samples <- levels(snv_long$sample_name)
+
+indel_long <- mutation_counts_by_type_ins_del %>%
+  mutate(sample_name = factor(sample_name, levels = ordered_samples))
+
+ggplot(indel_long, aes(x = sample_name, y = num_mutations, fill = type)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  scale_fill_manual(values = color_palette[c(1, 3)]) +
+  theme_minimal() +
+  labs(
+    title = " ",
+    y = "Indel count",
+    x = "Sample"
+  ) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.title = element_blank(),
+    panel.grid.major = element_blank(),  
+    panel.grid.minor = element_blank(),
+    legend.position = c(0.9, 0.85)
+  )
